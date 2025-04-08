b1613a5d6d16157e226e280fcb98db22c0363f0d
admin <admin>
2025-04-08 11:37:15 +0200

    Edited file 'python-lib/backend/fetch_api.py'

--- python-lib/backend/fetch_api.py
+++ python-lib/backend/fetch_api.py
@@ -38,7 +38,7 @@ if not is_local:
     if visual_ml_config.create_new_analysis:
         visual_ml_trainer.create_initial_ml_task()
         logger.info(f"Created new analysis {visual_ml_config.experiment_name}")
-        abort(200)
+        abort(Response(f"Created new analysis {visual_ml_config.experiment_name}. Now select and restart backend", 500))
     else:
         visual_ml_trainer.setup_using_existing_ml_task(
             visual_ml_config.existing_analysis_id
