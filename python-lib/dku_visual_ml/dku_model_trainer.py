20e30c8b7006b14912a3fe93ebcef2b3acac3f58
admin <admin>
2025-04-09 18:50:55 +0200

    Edited file 'python-lib/dku_visual_ml/dku_model_trainer.py'

--- python-lib/dku_visual_ml/dku_model_trainer.py
+++ python-lib/dku_visual_ml/dku_model_trainer.py
@@ -99,7 +99,7 @@ class VisualMLModelTrainer(DataikuClientProject):
         
         analysis = self.project.get_analysis(analysis_id)
         new_analysis_defintion = analysis.get_definition().get_raw()
-        experiment_name = str(self.visual_ml_config.analysis_name)
+        analysis_name = str(self.visual_ml_config.analysis_name)
         new_analysis_defintion['name'] = str(self.visual_ml_config.input_dataset) + "_" + analysis_name
         self.visual_ml_config.analysis_id = analysis_id
         analysis_definition = analysis.set_definition(new_analysis_defintion)
