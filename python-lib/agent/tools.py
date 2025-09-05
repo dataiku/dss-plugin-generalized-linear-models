9aadc579df4e2ac7330d4577c4ed4480661c6eb1
davidbehar <david.behar@dataiku.com>
2025-09-05 14:44:46 +0200

    add tools

---
+++ python-lib/agent/tools.py
@@ -0,0 +1,323 @@
+from backend.services import DataikuDataService
+
+service = DataikuDataService()
+
+def train_model(
+	ml_task_id,
+	analysis_id,
+	targetColumn=None,
+	exposureColumn=None,
+	splitPolicy=None,
+	trainSet=None,
+	analysisName=None,
+	testSet=None,
+	model_parameters=None,
+	variables=None,
+	interaction_variables=None
+):
+	"""
+	Train a model using the DataikuDataService.
+	Args:
+		ml_task_id: The ML task ID.
+		analysis_id: The analysis ID.
+		targetColumn: (optional) Target column name.
+		exposureColumn: (optional) Exposure column name.
+		model_parameters: (optional) Dict of model parameters (distribution_function, link_function, etc).
+		variables: (optional) Dict of variables.
+		interaction_variables: (optional) List of interaction variables.
+	Returns:
+		Service response dict.
+	"""
+	request_json = dict(
+		ml_task_id=ml_task_id,
+		analysis_id=analysis_id,
+	)
+	if targetColumn is not None:
+		request_json['targetColumn'] = targetColumn
+	if exposureColumn is not None:
+		request_json['exposureColumn'] = exposureColumn
+	if splitPolicy is not None:
+		request_json['splitPolicy'] = splitPolicy
+	if trainSet is not None:
+		request_json['trainSet'] = trainSet
+	if analysisName is not None:
+		request_json['analysisName'] = analysisName
+	if testSet is not None:
+		request_json['testSet'] = testSet
+	if model_parameters is not None:
+		request_json['model_parameters'] = model_parameters
+	if variables is not None:
+		request_json['variables'] = variables
+	if interaction_variables is not None:
+		request_json['interaction_variables'] = interaction_variables
+	return service.train_model(request_json)
+
+def deploy_model(id, input_dataset, experiment_name):
+	"""
+	Deploy a model using the DataikuDataService.
+	Args:
+		id: Model ID.
+		input_dataset: Input dataset name.
+		experiment_name: Experiment name.
+	Returns:
+		Service response dict.
+	"""
+	request_json = dict(id=id, input_dataset=input_dataset, experiment_name=experiment_name)
+	return service.deploy_model(request_json)
+
+def delete_model(id, input_dataset, experiment_name):
+	"""
+	Delete a model using the DataikuDataService.
+	Args:
+		id: Model ID.
+		input_dataset: Input dataset name.
+		experiment_name: Experiment name.
+	Returns:
+		Service response dict.
+	"""
+	request_json = dict(id=id, input_dataset=input_dataset, experiment_name=experiment_name)
+	return service.delete_model(request_json)
+
+def get_latest_mltask_params(id):
+	"""
+	Get the latest ML task parameters for a model.
+	Args:
+		id: Model ID.
+	Returns:
+		Setup parameters dict.
+	"""
+	request_json = dict(id=id)
+	return service.get_latest_mltask_params(request_json)
+
+def get_variables(id):
+	"""
+	Get variables used in a model.
+	Args:
+		id: Model ID.
+	Returns:
+		List of variables.
+	"""
+	request_json = dict(id=id)
+	return service.get_variables(request_json)
+
+def get_models(mlTaskId, analysisId):
+	"""
+	Get all models for a given ML task and analysis.
+	Args:
+		mlTaskId: ML task ID.
+		analysisId: Analysis ID.
+	Returns:
+		List of models.
+	"""
+	request_json = dict(mlTaskId=mlTaskId, analysisId=analysisId)
+	return service.get_models(request_json)
+
+def get_predicted_base(id, trainTest, variable):
+	"""
+	Get predicted base values for a variable in a model.
+	Args:
+		id: Model ID.
+		trainTest: Boolean, True for train set, False for test set.
+		variable: Variable name.
+	Returns:
+		List of predicted base values.
+	"""
+	request_json = dict(id=id, trainTest=trainTest, variable=variable)
+	return service.get_predicted_base(request_json)
+
+def get_base_values(id):
+	"""
+	Get base values for a model.
+	Args:
+		id: Model ID.
+	Returns:
+		List of base values.
+	"""
+	request_json = dict(id=id)
+	return service.get_base_values(request_json)
+
+def get_lift_data(id, nbBins, trainTest):
+	"""
+	Get lift chart data for a model.
+	Args:
+		id: Model ID.
+		nbBins: Number of bins.
+		trainTest: Boolean, True for train set, False for test set.
+	Returns:
+		List of lift chart data.
+	"""
+	request_json = dict(id=id, nbBins=nbBins, trainTest=trainTest)
+	return service.get_lift_data(request_json)
+
+def get_relativities(id):
+	"""
+	Get relativities for a model.
+	Args:
+		id: Model ID.
+	Returns:
+		List of relativities.
+	"""
+	request_json = dict(id=id)
+	return service.get_relativities(request_json)
+
+def get_variable_level_stats(id):
+	"""
+	Get variable level statistics for a model.
+	Args:
+		id: Model ID.
+	Returns:
+		List of variable level stats.
+	"""
+	request_json = dict(id=id)
+	return service.get_variable_level_stats(request_json)
+
+def get_model_metrics(id):
+	"""
+	Get model metrics (AIC, BIC, Deviance) for a model.
+	Args:
+		id: Model ID.
+	Returns:
+		Dict of metrics.
+	"""
+	request_json = dict(id=id)
+	return service.get_model_metrics(request_json)
+
+def export_model(id):
+	"""
+	Export a model as CSV.
+	Args:
+		id: Model ID.
+	Returns:
+		CSV bytes.
+	"""
+	request_json = dict(id=id)
+	return service.export_model(request_json)
+
+def export_variable_level_stats(id):
+	"""
+	Export variable level stats as CSV.
+	Args:
+		id: Model ID.
+	Returns:
+		CSV bytes.
+	"""
+	request_json = dict(id=id)
+	return service.export_variable_level_stats(request_json)
+
+def export_lift_chart(id, nbBins, trainTest):
+	"""
+	Export lift chart as CSV.
+	Args:
+		id: Model ID.
+		nbBins: Number of bins.
+		trainTest: Boolean, True for train set, False for test set.
+	Returns:
+		CSV bytes.
+	"""
+	request_json = dict(id=id, nbBins=nbBins, trainTest=trainTest)
+	return service.export_lift_chart(request_json)
+
+def export_one_way(id, variable, trainTest, rescale):
+	"""
+	Export one-way chart as CSV.
+	Args:
+		id: Model ID.
+		variable: Variable name.
+		trainTest: Boolean, True for train set, False for test set.
+		rescale: Rescale option.
+	Returns:
+		CSV bytes.
+	"""
+	request_json = dict(id=id, variable=variable, trainTest=trainTest, rescale=rescale)
+	return service.export_one_way(request_json)
+
+def get_dataset_columns(dataset, exposure):
+	"""
+	Get dataset columns and base levels.
+	Args:
+		dataset: Dataset name.
+		exposure: Exposure column name.
+	Returns:
+		List of columns and base levels.
+	"""
+	request_json = dict(dataset=dataset, exposure=exposure)
+	return service.get_dataset_columns(request_json)
+
+def get_project():
+	"""
+	Get the current project key.
+	Returns:
+		Dict with projectKey.
+	"""
+	return service.get_project()
+
+def get_ml_tasks():
+	"""
+	Get all ML tasks in the project.
+	Returns:
+		List of ML tasks.
+	"""
+	return service.get_ml_tasks()
+
+def get_datasets():
+	"""
+	Get all datasets in the project.
+	Returns:
+		List of datasets.
+	"""
+	return service.get_datasets()
+
+
+def create_ml_task(
+	targetColumn=None,
+	exposureColumn=None,
+	splitPolicy=None,
+	trainSet=None,
+	analysisName=None,
+	testSet=None,
+	variables=None,
+	interaction_variables=None
+):
+	"""
+	Create a new ML task using splitPolicy-related parameters.
+	Args:
+		targetColumn: (optional) Target column name.
+		exposureColumn: (optional) Exposure column name.
+		splitPolicy: (optional) Split policy string.
+		trainSet: (optional) Training set name.
+		analysisName: (optional) Analysis name.
+		testSet: (optional) Test set name.
+		variables: (optional) Dict of variables.
+		interaction_variables: (optional) List of interaction variables.
+	Returns:
+		Service response dict.
+	"""
+	request_json = {}
+	if targetColumn is not None:
+		request_json['targetColumn'] = targetColumn
+	if exposureColumn is not None:
+		request_json['exposureColumn'] = exposureColumn
+	if splitPolicy is not None:
+		request_json['splitPolicy'] = splitPolicy
+	if trainSet is not None:
+		request_json['trainSet'] = trainSet
+	if analysisName is not None:
+		request_json['analysisName'] = analysisName
+	if testSet is not None:
+		request_json['testSet'] = testSet
+	if variables is not None:
+		request_json['variables'] = variables
+	if interaction_variables is not None:
+		request_json['interaction_variables'] = interaction_variables
+	return service.create_ml_task(request_json)
+
+def get_variables_for_dataset(name):
+	"""
+	Get variables for a given dataset.
+	Args:
+		name: Dataset name.
+	Returns:
+		List of variables.
+	"""
+	request_json = dict(name=name)
+	return service.get_variables_for_dataset(request_json)
